af1d166de84b15377ccc1904263d31911f22c77d
Bo Chen <chen.bo2@northeastern.edu>
2021-12-17 02:16:56 -0800

    build api

--- index.js
+++ index.js
@@ -2,11 +2,23 @@ const express = require('express')
 const mongoose = require('mongoose')
 const bodyParser = require('body-parser')
 
+
+// init the app
 const app = express()
+
+// connect to mongodb
+mongoose.connect('mongodb+srv://chenbonick:Nick123456789@cluster0.gkbbl.mongodb.net/job-board')
+mongoose.Promise = global.Promise
 
 app.use(bodyParser.json())
 
+// init routes
 app.use('/api', require('./routes/api'))
+
+// Error handling
+app.use(function (err, req, res, next) {
+    res.status(422).send({error: err.message})
+})
 
 app.listen(process.env.port || 4000, function () {
     console.log('ready for accept request')
--- routes/api.js
+++ routes/api.js
@@ -1,30 +1,51 @@
 const express = require('express')
-
 const router = express.Router()
-
-//const Job = ''
+const Job = require('../models/job')
 
 
 // get all jobs
 router.get('/jobs', function(req, res, next) {
-    console.log('lets go fetch all jobs')
-    res.send('see all jobs')
+    Job.find({}).then(function (jobs) {
+        res.send(jobs)
+    }).catch(next)
 })
 
 
+
 //get one job
-router.get()
+router.get('/jobs/:id', function(req, res, next) {
+    Job.findOne({_id: req.params.id}).then(function (job) {
+        res.send(job)
+    }).catch(next)
+})
 
 
 // add new job
-router.post()
+router.post('/jobs', function(req, res, next) {
+    Job.create(req.body)
+    .then(function (job) {
+        res.send(job)
+    }).catch(next)
+})
 
 
 // update job
-router.put()
+router.put('/jobs/:id', function(req, res, next){
+    Job.findByIdAndUpdate({_id: req.params.id}, req.body)
+    .then(function () {
+        Job.findOne({_id: req.params.id}).then(function (job) {
+            res.send(job)
+        })
+    }).catch(next)
+})
 
 
-// delete jon
-router.delete()
+// delete job
+router.delete('/jobs/:id', function(req, res, next){
+    Job.findByIdAndRemove({_id: req.params.id})
+    .then(function (job) {
+        res.send(job)
+    }).catch(next)
+})
 
 module.exports = router
